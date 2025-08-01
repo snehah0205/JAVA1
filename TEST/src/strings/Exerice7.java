9360ebd8a1923f01db6f50c3e0ff3deb5e522cc9
sneha <sneha@SNEHA>
2025-08-01 19:12:31 +0530

    first commit

---
+++ TEST/src/strings/Exerice7.java
@@ -0,0 +1,14 @@
+package strings;
+public class Exerice7 { 
+    public static void main(String[] args) 
+    { 
+        String str1 = "PHP Exercises and "; 
+        String str2 = "Python Exercises"; 
+        System.out.println("String 1: " + str1); 
+        System.out.println("String 2: " + str2);  
+           // Concatenate the two strings together. 
+        String str3 = str1.concat(str2); 
+        // Display the new String. 
+        System.out.println("The concatenated string: " + str3); 
+    } 
+} 
